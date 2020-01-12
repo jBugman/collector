98d0e1ce1bdf414898f1e337e31c9e5063d1cdcd
Sergey Parshukov <codedby@bugman.me>
2020-01-12 13:55:23 +0800

    External global CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,4 @@
-import { injectGlobal } from 'emotion';
 import ItemStats from './components/ItemStats';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-injectGlobal`
-  * {
-    box-sizing: border-box;
-  }
-  html, body, #root {
-    height: 100%;
-    margin: 0;
-  }
-  body {
-    font-family: Verdana, Arial, sans-serif;
-    font-size: 13px;
-    color: #A38D6D;
-    background: #000;
-  }
-  #root {
-    display: grid;
-  }
-`;
 
 const App = () => (
   <ItemStats/>
