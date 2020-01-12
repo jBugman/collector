3364a9830f3a9daf297df72781e7677678056e20
Sergey Parshukov <codedby@bugman.me>
2020-01-12 10:28:45 +0800

    Migrate to Parcel

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,21 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
-    'standard'
+    'standard',
   ],
   plugins: [
     '@typescript-eslint',
-    'react'
+    'react',
   ],
   rules: {
-    'semi': ['error', 'always'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', {
+      arrays: 'only-multiline',
+      objects: 'only-multiline',
+    }],
     '@typescript-eslint/explicit-function-return-type': 'off',
     // Fixes JSX with Solid
     'react/jsx-uses-react': 'error',
-    'react/jsx-uses-vars': 'error'
-  }
+    'react/jsx-uses-vars': 'error',
+  },
 };
